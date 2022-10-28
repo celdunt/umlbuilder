c56d37625760ddf9d4a8f79a359f973ed57120c6
celdunt <s1zceero@gmail.com>
2022-10-28 14:31:07 +0300

    update

---
+++ src/main/java/com/celdunt/umlbuilder/UMLBuilderAction.java
@@ -0,0 +1,115 @@
+package com.celdunt.umlbuilder;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.fileChooser.FileChooser;
+import com.intellij.openapi.fileChooser.FileChooserDescriptor;
+import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiField;
+import com.intellij.psi.PsiMethod;
+import com.intellij.ui.JBColor;
+import org.jetbrains.annotations.NotNull;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Objects;
+
+import static com.intellij.util.ui.ImageUtil.createImage;
+
+public class UMLBuilderAction extends AnAction {
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        final int widthWindow = 1000;
+        final int heightWindow = 1000;
+
+        final VirtualFile chooserVFile = getChooserVirtualFile(e);
+
+        final BufferedImage bufferedImage = createImage(widthWindow, heightWindow, BufferedImage.TYPE_INT_RGB);
+        final Graphics2D g2d = createGraphics(bufferedImage, JBColor.white, widthWindow, heightWindow);
+
+        Object[] selectedClasses = e.getRequiredData(PlatformDataKeys.SELECTED_ITEMS);
+
+        drawUML(selectedClasses, g2d);
+        if (chooserVFile != null) saveGraphicsAsImage(bufferedImage, chooserVFile.getPath());
+    }
+
+    private void drawUML(Object[] selectedClasses, Graphics2D g2d) {
+        ArrayList<String> fieldsArray = new ArrayList<>();
+        ArrayList<String> methodsArray = new ArrayList<>();
+
+        ArrayList<UMLClass> classes = new ArrayList<>();
+
+        int coordX = 50;
+        int coordY = 50;
+
+        for (Object classUnit : selectedClasses) {
+            System.out.println(classUnit.toString());
+            if (classUnit.toString().contains("PsiClass")) {
+                for (PsiField field : ((PsiClass) classUnit).getFields())
+                    fieldsArray.add("-" + field.getName() + "  " + field.getType().toString().replace("PsiType", ""));
+
+                for (PsiMethod method : ((PsiClass) classUnit).getMethods())
+                    methodsArray.add("+" + method.getName() + "  " + Objects.requireNonNull(method.getReturnTypeElement()).toString().replace("PsiTypeElement", ""));
+
+                System.out.println(fieldsArray.size());
+                System.out.println(methodsArray.size());
+
+                final String className = ((PsiClass) classUnit).getName();
+                final String classType = getInActionClassType((PsiClass) classUnit);
+
+                classes.add(new UMLClass()
+                        .defX(coordX)
+                        .defY(coordY)
+                        .defType(classType)
+                        .defName(className)
+                        .defFields(fieldsArray)
+                        .defMethods(methodsArray));
+
+                coordX += 200;
+
+                fieldsArray.clear();
+                methodsArray.clear();
+            }
+        }
+
+        for (UMLClass classItem : classes)
+            classItem.draw(g2d);
+    }
+    private Graphics2D createGraphics(BufferedImage bufferedImage, Color background, int width, int height) {
+        final Graphics2D g2d = bufferedImage.createGraphics();
+
+        g2d.setPaint(background);
+        g2d.setColor(background);
+        g2d.fillRect(0, 0, width, height);
+
+        return g2d;
+    }
+    private String getInActionClassType(PsiClass item) {
+        return item.isInterface()? UMLClass.interfaceType:
+                Objects.requireNonNull(item.getModifierList()).toString().contains("abstract")? UMLClass.abstractType:
+                        UMLClass.classType;
+    }
+    private VirtualFile getChooserVirtualFile(AnActionEvent event) {
+        FileChooserDescriptor descriptor = FileChooserDescriptorFactory.createSingleFileDescriptor();
+        Project project = getEventProject(event);
+        VirtualFile virtualFile = null;
+
+        return FileChooser.chooseFile(descriptor, project, virtualFile);
+    }
+    private void saveGraphicsAsImage(BufferedImage bufferedImage, String path) {
+        try {
+            ImageIO.write(bufferedImage, "png", new File(path + "/image.png"));
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/celdunt/umlbuilder/UMLClass.java
@@ -0,0 +1,155 @@
+package com.celdunt.umlbuilder;
+
+import com.intellij.ui.JBColor;
+
+import java.awt.*;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.AffineTransform;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class UMLClass extends UMLFigure{
+    public static final String abstractType = "A";
+    public static final String classType = "C";
+    public static final String interfaceType = "I";
+
+    private final String abstractHexCode = "#5d8aa8";
+    private final String classHexCode = "#915c83";
+    private final String interfaceHexCode = "#9966cc";
+
+    private int widthOval = 25;
+    private int heightOval = 25;
+    private int margin = 6;
+
+    private String type = classType;
+    private String name = "Default";
+    private ArrayList<String> fields = new ArrayList<>();
+    private ArrayList<String> methods = new ArrayList<>();
+
+
+    private final Font font = new Font("Arial", Font.PLAIN, 10);
+    private final FontRenderContext fontRenderContext = new FontRenderContext(new AffineTransform(), true, true);
+
+
+    @Override
+    public void draw(Graphics2D g2d) {
+        g2d.setColor(JBColor.black);
+
+        calculateSizeClassRectangle();
+
+        g2d.drawRect(this.x, this.y, (int)this.width, (int)this.height);
+
+        switch (type) {
+            case classType: drawTypeClassCircle(g2d, type, classHexCode);
+            case abstractType: drawTypeClassCircle(g2d, type, abstractHexCode);
+            case interfaceType: drawTypeClassCircle(g2d, type, interfaceHexCode);
+        }
+
+        drawContentClass(g2d);
+    }
+
+    private int getTextWidth(String text) {
+        return (int)font.getStringBounds(text, fontRenderContext).getWidth();
+    }
+    private int getTextHeight(String text) {
+        return (int)font.getStringBounds(text, fontRenderContext).getHeight();
+    }
+    private void calculateSizeClassRectangle() {
+        AtomicInteger widthFields = new AtomicInteger(0);
+        AtomicInteger heightFields = new AtomicInteger(0);
+
+        calculateMaxSize(widthFields, heightFields, fields);
+
+        AtomicInteger widthMethods = new AtomicInteger(0);
+        AtomicInteger heightMethods = new AtomicInteger(0);
+
+        calculateMaxSize(widthMethods, heightMethods, methods);
+
+        int widthContent = Integer.max(widthFields.get(), widthMethods.get());
+        int heightContent = Integer.max(heightFields.get(), heightMethods.get());
+
+        if (getTextWidth(name) > widthContent) widthContent = getTextWidth(name) + margin;
+
+        if (widthOval + 2 * margin + widthContent > this.width)
+            this.width = widthOval + 4 * margin + widthContent;
+
+        if (heightOval + 2 * margin + getTextHeight(name) + heightContent > this.height)
+            this.height = heightOval + 5 * margin + getTextHeight(name) + heightContent;
+    }
+    private void calculateMaxSize(AtomicInteger width, AtomicInteger height, ArrayList<String> listString) {
+        for (String item : listString) {
+            height.addAndGet(getTextHeight(item));
+            width.set(Integer.max(width.get(), getTextWidth(item)));
+        }
+    }
+    private void drawTypeClassCircle(Graphics2D g2d, String type, String hexCode) {
+        g2d.setColor(Color.decode(hexCode));
+        g2d.fillOval(this.x + margin, this.y + margin, widthOval, heightOval);
+
+        g2d.setColor(JBColor.black);
+
+        final int posInnerX = -getTextWidth(type) / 2 + margin - 1;
+        final int posInnerY = -getTextHeight(type) / 2 + margin / 2 + 1;
+
+        g2d.drawString(type, this.x + widthOval / 2 + posInnerX, this.y + getTextHeight(type) + heightOval / 2 + posInnerY);
+        g2d.drawString(name, this.x + margin + widthOval + margin, this.y + getTextHeight(name) + 2 * margin);
+    }
+    private void drawContentClass(Graphics2D g2d) {
+        AtomicInteger currentY = new AtomicInteger(this.y + 2 * heightOval);
+
+        g2d.setColor(JBColor.black);
+
+        drawContentClassStrings(g2d, currentY, fields);
+
+        currentY.addAndGet(margin);
+
+        drawContentClassStrings(g2d, currentY, methods);
+    }
+    private void drawContentClassStrings(Graphics2D g2d, AtomicInteger currentY, ArrayList<String> listString) {
+        for (String item : listString) {
+            g2d.drawString(item, (int)(this.x + this.width / 2 - getTextWidth(item) / 2), currentY.get());
+            currentY.addAndGet(getTextHeight(item));
+        }
+    }
+
+    public UMLClass defWidth(double width) {
+        this.width = width;
+        return this;
+    }
+    public UMLClass defHeight(double height) {
+        this.height = height;
+        return this;
+    }
+    public UMLClass defColor(Color color) {
+        this.color = color;
+        return this;
+    }
+    public UMLClass defX(int x) {
+        this.x = x;
+        return this;
+    }
+    public UMLClass defY(int y) {
+        this.y = y;
+        return this;
+    }
+    public UMLClass defType(String type) {
+        if (Objects.equals(type, UMLClass.classType) ||
+                Objects.equals(type, UMLClass.abstractType) ||
+                Objects.equals(type, UMLClass.interfaceType))
+            this.type = type;
+        return this;
+    }
+    public UMLClass defName(String name) {
+        this.name = name;
+        return this;
+    }
+    public UMLClass defFields(ArrayList<String> fields) {
+        this.fields.addAll(fields);
+        return this;
+    }
+    public UMLClass defMethods(ArrayList<String> methods) {
+        this.methods.addAll(methods);
+        return this;
+    }
+}
